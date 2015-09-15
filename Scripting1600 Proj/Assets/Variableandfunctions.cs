80282c1f1566d239ce81a7be9b1f8115bb8655ca
Josh Ells <joshey_kun19@yahoo.com>
2015-09-15 09:15:56 -0600

    Pushing it all again

---
+++ Scripting1600 Proj/Assets/Variableandfunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variableandfunctions : MonoBehaviour 
+	{
+	int myInt = 8;
+	void Start ()
+	{
+		myInt = dividebytwo (myInt);
+		Debug.Log (myInt);
+	}
+	int dividebytwo (int number)
+	{
+		int ret;
+		ret = number - 6;
+		return ret;
+	}
+
+
+
+	}
---
+++ Scripting1600 Proj/Assets/Variables and Functions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesandFunctions : MonoBehaviour 
+{
+	int myInt = 8;
+
+	void Start () 
+	{
+		MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	int MultiplyByTwo(int number) {
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+		
+}
