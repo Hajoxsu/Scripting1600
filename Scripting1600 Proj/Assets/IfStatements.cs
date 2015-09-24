256bd1393dc57d96902fef941be7b7b98fabee34
Josh Ells <joshey_kun19@yahoo.com>
2015-09-24 08:56:03 -0600

    Passed off If Statements

---
+++ Scripting1600 Proj/Assets/IfStatements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+	public float TooSlowSpeed = 65f;
+	public float TooFastSpeed = 75f;
+	public float carSpeed = 0f;
+
+	// Use this for initialization
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			SpedTest ();
+		carSpeed += Time.deltaTime * 5f;
+	}
+	
+	// Update is called once per frame
+	void SpedTest () {
+		if (carSpeed > TooFastSpeed) {
+			print ("Too fast! Slow Down!");
+		}
+		else if (carSpeed < TooSlowSpeed){
+			print ("Too slow! Speed up!");
+		}
+		else {
+			print ("Good Speed! Keep going!");
+		}
+	}
+}
