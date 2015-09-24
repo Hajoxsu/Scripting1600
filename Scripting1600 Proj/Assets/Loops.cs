69f4cbc0f8a0da22b97bf2adcebabe34f57403e9
Josh Ells <joshey_kun19@yahoo.com>
2015-09-24 09:42:17 -0600

    Loops

---
+++ Scripting1600 Proj/Assets/Loops.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+	//This is a For Loop.
+	int numEnemies = 4;
+	
+	void Start () {
+		for(int i = 0; i < numEnemies; i++) 
+		{
+			Debug.Log("Creating enemy number" + " " + i);
+		}
+	}
+
+}
