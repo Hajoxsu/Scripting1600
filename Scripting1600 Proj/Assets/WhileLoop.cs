7f594fb706d9989684df6ebeb1fb3d08dc1e8624
unknown <10546874@CS514-07.ad.uvu.edu>
2015-10-01 09:36:38 -0600

    WhileLoop

---
+++ Scripting1600 Proj/Assets/WhileLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour {
+
+
+	int catsInTheGround = 3;
+
+	void Start () 
+	{
+		while(catsInTheGround > 0);
+		{
+			Debug.Log("I've buried a cat!");
+			catsInTheGround--;
+		}
+	}
+}
