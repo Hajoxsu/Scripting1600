2c1beea73429bcade298afb4b1b6955d91ae366e
Josh Ells <joshey_kun19@yahoo.com>
2015-10-08 09:38:41 -0600

    no message

---
+++ Scripting1600 Proj/Assets/AnotherClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass : MonoBehaviour {
+
+	public int raspberries;
+	public int strawberries;
+	
+	
+	private int sharpener;
+	private int tape;
+	
+	
+	public void FruitMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+	
+	
+	private void OfficeSort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Office Supplies total: " + answer);
+	}
+}
---
+++ Scripting1600 Proj/Assets/ScopeandAccessMod.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeandAccessMod : MonoBehaviour {
+
+	public int alpha = 7;
+	
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	
+	private AnotherClass myOtherClass;
+	
+	
+	void Start ()
+	{
+		alpha = 32;
+		
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(alpha,myOtherClass.raspberries);
+	}
+	
+	
+	void Example (int pencils, int erasers)
+	{
+		int answer;
+		answer = pencils * erasers * alpha;
+		Debug.Log(answer);
+	}
+	
+	
+	void Update ()
+	{
+		Debug.Log("Alpha is set to: " + alpha);
+	}
+}
