dc07ba1e9ee82c041cea5c880481732ddd55df87
Josh Ells <joshey_kun19@yahoo.com>
2015-11-10 09:09:47 -0700

    no message

---
+++ Scripting1600 Proj/Assets/Enumerations.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumerations : MonoBehaviour 
+{
+	public enum Direction{North, East, South, West};
+	
+	void Start ()
+	{
+		Direction myDirection = ReverseDirection(Direction.East);
+		
+		Debug.Log (myDirection);
+	}
+	
+	Direction ReverseDirection (Direction dir)
+	{
+		if(dir == Direction.North)
+			dir = Direction.South;
+		else if(dir == Direction.South)
+			dir = Direction.North;
+		else if(dir == Direction.East)
+			dir = Direction.West;
+		else if(dir == Direction.West)
+			dir = Direction.East;
+		
+		return (dir);
+		
+	
+	}
+}
---
+++ Scripting1600 Proj/Assets/SwitchStatements.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchStatements : MonoBehaviour {
+	public int intelligence = 5;
+
+	void Greet () 
+	{
+		switch (intelligence)
+		{
+		case 5:
+			print("Well good day! Let me teach you about the solar system!");
+			break;
+		case 4:
+			print("Hello! How are you?");
+			break;
+		case 3:
+			print("Wuz up?");
+			break;
+		case 2:
+			print("Bane smash!");
+			break;
+		case 1:
+			print("Wockie wock wock sock");
+			break;
+		default: 
+			print("Incorrect intelligence level.");
+			break;
+		}
+	}
+
+	void Update ()
+	{
+		Debug.Log (intelligence);
+	}
+}
+
+
